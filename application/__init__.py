112c220c52506382de47b79acd20721c3544c75c
art-aliu <art.aliu@googlemail.com>
2022-04-05 13:02:59 +0000

    art:added routes.py, init.py, app.py, create.py

--- application/__init__.py
+++ application/__init__.py
@@ -5,7 +5,7 @@ import pymysql
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:Liverlona1@34.105.148.169:3306/project"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:Liverlona1@34.105.148.169:3306/project-db"
 
 db = SQLAlchemy(app)
 

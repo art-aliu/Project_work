d62c1b1be88d5bf20c7db927c62e9eb2f23d01c3
art-aliu <art.aliu@googlemail.com>
2022-04-08 12:26:19 +0000

    Art:changes made to models to make it more simple and easy to do

--- application/models.py
+++ application/models.py
@@ -1,6 +1,5 @@
 
 from application import db
-from application import app
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField, SelectField
 from wtforms.validators import DataRequired
@@ -10,31 +9,26 @@ class Football_Team(db.Model):
     __tablename__ = 'football_team'
     id = db.Column(db.Integer, primary_key=True)
     football_team_name = db.Column(db.String(45), nullable=False)
-    player = db.relationship('Player', backref='football_teamsbr')
+    football_team_player = db.relationship('Player', backref='playerbr')
 
 def __repr__(self):
         return 'Choose {}'.format(self.football_team_name)
 
 class Player(db.Model):    
-    __tablename__ = 'player'
     id = db.Column(db.Integer, primary_key=True)
     player_name = db.Column(db.String(45), nullable=False)
-    player_number = db.Column(db.Integer, nullable=False)
     player_age = db.Column(db.Integer, nullable=False)
     football_team_id = db.Column(db.Integer, db.ForeignKey('football_team.id'), nullable=False)
 
 
 class AddFootball_TeamForm(FlaskForm):
-    football_team_name = StringField('Enter the football team name', validators=[DataRequired()])
+    football_team_name = StringField('Football team name', validators=[DataRequired()])
     submit = SubmitField ('Submit the name')
 
 
 class AddPlayerForm(FlaskForm):
-    player_name = StringField ('Enter player name')
-    player_number = IntegerField ('Enter the player number')
+    player_name = StringField('Player Name', validators=[DataRequired()])
     player_age = IntegerField ('Enter the player age')
-    football_team_name = SelectField ('Football Club', choices = [])
+    football_team_name = SelectField (u'Football Team Name', choices = [])
     submit = SubmitField ('Submit')
-
-
 
--- application/routes.py
+++ application/routes.py
@@ -1,26 +1,3 @@
-# @app.route('/add')
-# def add():
-#     new_player = Players(name="New Player")
-#     db.session.add(new_player)
-#     db.session.commit()
-#     return "Added new player to database"
-
-# @app.route('/read')
-# def read():
-#     all_players = players.query.all()
-#     players_string = ""
-#     for player in all_players:
-#         players_string += "<br>"+ player.name
-#     return players_string
-
-# @app.route('/update/<name>')
-# def update(name):
-#     first_player = player.query.first()
-#     first_player.name = name
-#     db.session.commit()
-#     return first_player.name
-
-
 from application import app, db
 from application.models import Player, Football_Team, AddPlayerForm, AddFootball_TeamForm
 from flask import render_template, redirect, url_for, request
@@ -29,14 +6,6 @@ from flask import render_template, redirect, url_for, request
 app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
 app.config['SQLALCHEMY_POOL_TIMEOUT'] = 600 
 app.config['SQLALCHEMY_POOL_RECYCLE'] = 100
-
-
-# @app.route('/', methods=['GET', 'POST'])
-# def index1():
-#     return render_template('index.html')
-# @app.route('/index,', methods=['GET', 'POST'])
-# def index():
-#     return render_template('index.html',)
 
 @app.route('/')
 def home():
@@ -108,45 +77,6 @@ def edit_football_team(football_team_name):
     else:
         return render_template('edit_one_football_team.html', edit_one_football_team=edit_one_football_team, form=form)
 
-# @app.route('/players')
-# def read():
-#     all_players = players.query.all()
-#     players_string = ""
-#     for players in all_players:
-#         players_string += "<br>"+ players.name
-#     return players_string
-
-# @app.route('/update_player/<name1>', methods=['GET', 'POST'])
-# def update_player(name1):
-#     form = AddPlayerForm()
-#     player = Player(name=name1)
-
-#     return render_template('update_player.html', player=player, form=form)
-
-# @app.route('/update_player2/<name2>', methods=['GET', 'POST'])
-# def update_player2(name2):
-#     updated_player = db.session.query(Player).filter_by(name=name2).first()
-#     updated_player.name = request.form.get('name')
-#     updated_player.player_age = request.form.get('player_age')
-#     db.session.commit()
-#     return redirect('/players')
-
-# @app.route('/update_player3/<name3>', methods=['GET', 'POST'])
-# def update_player3(name3):
-#     updated_player = db.session.query(Player).filter_by(name=name3).first()
-#     updated_player.name = request.form.get('name')
-#     updated_player.player_age = request.form.get('player_age')
-#     updated_player.player_number = request.form.get('player_number')
-#     db.session.commit()
-#     return redirect('/players')
-
-
-# @app.route('/update_football_team/<name>/<manager>', methods=['GET', 'POST'])
-# def update_football_team(name,):
-#     form = Football_TeamForm()
-#     Football_Team = Football_Team(name=name)
-
-#     return render_template('update_football_team.html', football_team=football_team, form=form)
 
 
 
